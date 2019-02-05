afe8333ca1a3bf1d98672a754069c60613559f15
mahshadul <mahshadul@gmail.com>
2019-02-05 13:20:38 -0500

    initial commit

---
+++ web-server.sh
@@ -0,0 +1,14 @@
+# # Update the apt-get cache
+# sudo apt-get Update
+
+# # install PHP
+# sudo apt-get install -y php 
+
+# # Install Apache
+# sudo apt-get install -y apache2 
+
+# # Copy the code from the repository
+# sudo git clone https://github.com/brikis98/php-app.git /var/www/html/app 
+
+# # start apache
+# sudo service apache2 start
